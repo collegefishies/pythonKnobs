2d0b1bcd478208ff0c767ab05dbb31d7e7c8fadb
collegefishies <enrique.krft@gmail.com>
2015-12-27 15:40:06 -1000

    Add analogRead

--- pythonKnobs/pythonKnobs.ino
+++ pythonKnobs/pythonKnobs.ino
@@ -6,9 +6,17 @@ void setup() {
 	pinMode(A2, INPUT);
 	pinMode(A3, INPUT);
 	pinMode(A4, INPUT);
+	pinMode(A5, INPUT);
 }
 
 void loop() {
+int knob1 = 0, knob2 = 0, knob3 = 0, knob4 = 0,knob5 = 0;
 
+knob1 = analogRead(A0);
+knob2 = analogRead(A1);
+knob3 = analogRead(A2);
+//A3 not connected
+knob4 = analogRead(A4);
+knob5 = analogRead(A5);
 }
 
